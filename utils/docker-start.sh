a7cdbb999f180abbe52c4eb7fa4638c3527d2ada
Tom Martensen <tom.martensen@student.hpi.de>
2017-05-23 21:06:21 +0200

    optimize docker experience

--- utils/docker-start.sh
+++ utils/docker-start.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
 cd /usr/src/app
+
+echo "Set environment variables"
+export SECRET_KEY=`date +%s | sha256sum | base64`
+cat << EOF > arionBackend/local_settings.py
+CORS_ORIGIN_WHITELIST = ["$HOST"]
+SECRET_KEY = "$SECRET_KEY"
+EOF
+
+echo "Making migrations to database"
 python manage.py makemigrations arionBackend
 python manage.py migrate
 echo "Starting server"
