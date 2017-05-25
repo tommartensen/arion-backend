bc295b14aea3d0e4b27bf21068358ac269e07e76
Tom Martensen <tom.martensen@student.hpi.de>
2017-05-25 12:47:31 +0200

    detects input streams, parses event streams

--- arionBackend/transformation/esper/esper_transformer.py
+++ arionBackend/transformation/esper/esper_transformer.py
@@ -66,24 +66,67 @@ class EsperTransformer(Transformer):
 		return updated_tokenized_query
 
 	@staticmethod
+	def find_selection(event_type):
+		try:
+			tokenized_event_type = parse("{name}.std:{selection}", event_type).named
+			return tokenized_event_type
+		except AttributeError:
+			return {"name": event_type}
+
+	@staticmethod
+	def find_alias(event_type):
+		if " as " in event_type:
+			tokenized_with_alias = event_type.split(" as ")
+			return {"event": tokenized_with_alias[0], "alias": tokenized_with_alias[1]}
+		else:
+			return {"event": event_type}
+
+	@staticmethod
+	def find_window(event_type):
+		try:
+			tokenized_event_type = parse("{name}.win:{type}", event_type["event"]["name"]).named
+			event_type["event"]["name"] = tokenized_event_type["name"]
+			tokenized_with_window = {"window": {"event": event_type["event"]}, "type": tokenized_event_type["type"]}
+			if "alias" in event_type:
+				tokenized_with_window["alias"] = event_type["alias"]
+			return tokenized_with_window
+		except AttributeError:
+			return event_type
+
+	@staticmethod
+	def tokenize_input_stream(input_stream):
+		tokenized_input = EsperTransformer.find_alias(input_stream)
+		tokenized_input["event"] = EsperTransformer.find_selection(tokenized_input["event"])
+		return EsperTransformer.find_window(tokenized_input)
+
+	@staticmethod
 	def parse_input_clause(input_clause):
 		"""
 		Parses the input clause: Single Event Types, Joins and Patterns are possible.
 		:param input_clause: the input clause to parse
 		:return: the updated input clause
 		"""
-		updated_input_clause = {}
+		updated_input_clause = {"input": {}}
 		if "," in input_clause:
 			# if a comma is in the input clause, multiple event streams are joined as input
-			updated_input_clause["join"] = [event_stream.strip() for event_stream in input_clause.split(',')]
+			stripped_event_streams = [event_stream.strip() for event_stream in input_clause.split(',')]
+			updated_input_clause["input"]["data"] = \
+				[EsperTransformer.tokenize_input_stream(event_stream) for event_stream in stripped_event_streams]
+			input_type = "JOIN"
 		else:
 			try:
 				tokenized_pattern = parse("pattern {pattern}", input_clause).named
-				updated_input_clause["pattern"] = EsperTransformer.parse_pattern(tokenized_pattern["pattern"].strip())
+				updated_input_clause["input"]["data"] = \
+					EsperTransformer.parse_pattern(tokenized_pattern["pattern"].strip())
+				input_type = "PATTERN"
 			# if no error is thrown, the pattern clause was detected
 			except AttributeError:
 				# input must be single event type
-				updated_input_clause["single"] = input_clause.strip()
+				updated_input_clause["input"]["data"] = input_clause.strip()
+				updated_input_clause["input"]["data"] = \
+					EsperTransformer.tokenize_input_stream(updated_input_clause["input"]["data"])
+				input_type = "SINGLE"
+		updated_input_clause["input"]["type"] = input_type
 		return updated_input_clause
 
 	@staticmethod
