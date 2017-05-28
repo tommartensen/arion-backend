5d10cdffebe5aea6e2add9dabbb2ad15bd49d208
Tom Martensen <tom.martensen@student.hpi.de>
2017-05-28 17:30:40 +0200

    added route for new api

--- arionBackend/api/query/urls.py
+++ arionBackend/api/query/urls.py
@@ -4,9 +4,11 @@ This module holds the url patterns for the query API.
 
 from django.conf.urls import url
 
-from arionBackend.api.query.query import GetQueriesByHierarchyId
+from arionBackend.api.query.query import GetQueriesByHierarchyId, GetQueryById
 
 urlpatterns = [
 	url(r'^esper/hierarchy/(?P<hierarchy_id>(\d){1,32})$',
 		GetQueriesByHierarchyId.as_view(), name="get-queries-by-hierarchy-id"),
+	url(r'^esper/(?P<query_id>(\d){1,32})$',
+	    GetQueryById.as_view(), name="get-query-by-id"),
 ]
