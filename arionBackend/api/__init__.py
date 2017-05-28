fbd60edfc48f2c00b1d2af2538d35df827409aa7
Tom Martensen <tom.martensen@student.hpi.de>
2017-05-28 17:29:24 +0200

    introduce parent class to reduce duplication in views

--- arionBackend/api/__init__.py
+++ arionBackend/api/__init__.py
@@ -1,3 +1,11 @@
 """
 This module contains all the APIs of the project.
 """
+from rest_framework.views import APIView
+
+
+class GetXByIdView(APIView):
+
+	@staticmethod
+	def validate_input(id):
+		return int(id)
