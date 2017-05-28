71a6928a52f64fcb703e9ba3af99d6a25912c123
Tom Martensen <tom.martensen@student.hpi.de>
2017-05-28 21:48:52 +0200

    fixing line breaks

--- arionBackend/api/event_type/urls.py
+++ arionBackend/api/event_type/urls.py
@@ -7,8 +7,7 @@ from django.conf.urls import url
 from arionBackend.api.event_type.event_type import GetEventTypesByHierarchyId, GetEventTypeById
 
 urlpatterns = [
-	url(r'^esper/hierarchy/(?P<hierarchy_id>(\d){1,32})$',
-	    GetEventTypesByHierarchyId.as_view(), name="get-event-types-by-hierarchy-id"),
-	url(r'^esper/(?P<event_type_id>(\d){1,32})$',
-	    GetEventTypeById.as_view(), name="get-event-type-by-id"),
+	url(r'^esper/hierarchy/(?P<hierarchy_id>(\d){1,32})$', GetEventTypesByHierarchyId.as_view(),
+		name="get-event-types-by-hierarchy-id"),
+	url(r'^esper/(?P<event_type_id>(\d){1,32})$', GetEventTypeById.as_view(), name="get-event-type-by-id"),
 ]
